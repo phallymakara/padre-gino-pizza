ff68003f1d6488dccc768bb5ec7b6849d660a3ae
phally makara <phallymakara01@gmail.com>
2026-03-13 09:27:26 +0700

    work with react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
-import React from "react";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
-      <Pizza
+      <Order
         name="The Hawaiian Pizza"
         description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/pepperoni.webp"}
       />
-      <Pizza
+      <Order
         name="The hongkong Pizza"
         description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
       />
-      <Pizza
+      <Order
         name="The Vietname Pizza"
         description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/big_meat.webp"}
       />
     </div>
   );
