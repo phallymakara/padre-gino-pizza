5174e223762954b83ab7f5d9a4b4846c83ed48fb
phally makara <phallymakara01@gmail.com>
2026-03-24 10:00:38 +0700

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,8 @@ import Order from "./Order";
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's Pizza – Order Now</h1>
-      <Order
-        name="The Hawaiian Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Order
-        name="The hongkong Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Order
-        name="The Vietname Pizza"
-        description="Sliced Ham, Pineapple, Mozzarella Cheese"
-        image={"/public/pizzas/big_meat.webp"}
-      />
+      <h1 className="logo">Padre Gino's Pizza</h1>
+      <Order />
     </div>
   );
 };
--- src/Order.jsx
+++ src/Order.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Pizza from "./Pizza";
 
+// feel free to change en-US / USD to your locale
 const intl = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
@@ -13,10 +14,11 @@ export default function Order() {
   const [loading, setLoading] = useState(true);
 
   let price, selectedPizza;
-
   if (!loading) {
     selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
-    price = intl.format(selectedPizza.sizes[pizzaSize]);
+    price = intl.format(
+      selectedPizza.sizes ? selectedPizza.sizes[pizzaSize] : "",
+    );
   }
 
   useEffect(() => {
@@ -24,11 +26,8 @@ export default function Order() {
   }, []);
 
   async function fetchPizzaTypes() {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const pizzasRes = await fetch("/api/pizzas");
     const pizzasJson = await pizzasRes.json();
-
     setPizzaTypes(pizzasJson);
     setLoading(false);
   }
@@ -36,16 +35,14 @@ export default function Order() {
   return (
     <div className="order">
       <h2>Create Order</h2>
-
       <form>
         <div>
           <div>
             <label htmlFor="pizza-type">Pizza Type</label>
-
             <select
+              onChange={(e) => setPizzaType(e.target.value)}
               name="pizza-type"
-              value={pizzaType}
-              onChange={(e) => setPizzaType(e.target.value)}>
+              value={pizzaType}>
               {pizzaTypes.map((pizza) => (
                 <option key={pizza.id} value={pizza.id}>
                   {pizza.name}
@@ -53,13 +50,12 @@ export default function Order() {
               ))}
             </select>
           </div>
-
           <div>
             <label htmlFor="pizza-size">Pizza Size</label>
-
-            <div onChange={(e) => setPizzaSize(e.target.value)}>
+            <div>
               <span>
                 <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   checked={pizzaSize === "S"}
                   type="radio"
                   name="pizza-size"
@@ -68,9 +64,9 @@ export default function Order() {
                 />
                 <label htmlFor="pizza-s">Small</label>
               </span>
-
               <span>
                 <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   checked={pizzaSize === "M"}
                   type="radio"
                   name="pizza-size"
@@ -79,9 +75,9 @@ export default function Order() {
                 />
                 <label htmlFor="pizza-m">Medium</label>
               </span>
-
               <span>
                 <input
+                  onChange={(e) => setPizzaSize(e.target.value)}
                   checked={pizzaSize === "L"}
                   type="radio"
                   name="pizza-size"
@@ -92,12 +88,10 @@ export default function Order() {
               </span>
             </div>
           </div>
-
           <button type="submit">Add to Cart</button>
         </div>
-
         {loading ?
-          <h3>Loading...</h3>
+          <h3>LOADING …</h3>
         : <div className="order-pizza">
             <Pizza
               name={selectedPizza.name}
