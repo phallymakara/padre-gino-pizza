46268699335d48590efa9bdc419a766733cc98d5
phally makara <phallymakara01@gmail.com>
2026-03-24 11:01:14 +0700

    custome hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzzaOfTheDay";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="logo">Padre Gino's Pizza</h1>
-      <Order />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
